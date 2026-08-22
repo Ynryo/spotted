1538e43182d5e002f1460adffbf2f707adb6ef83
Ynryo <ewenatideklein@gmail.com>
2026-08-22 21:02:19 +0200

    bump: Update app version to 1.2.6 (versionCode 126) in `build.gradle.kts`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "fr.ynryo.spotted"
         minSdk = 30
         targetSdk = 36
-        versionName = "1.2.5"
-        versionCode = 125
+        versionName = "1.2.6"
+        versionCode = 126
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
