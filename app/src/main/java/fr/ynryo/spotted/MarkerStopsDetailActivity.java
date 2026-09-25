8ac15641d0f23d2743394539e34ef1cff77dc4b0
Ynryo <ewenatideklein@gmail.com>
2026-09-25 11:32:30 +0200

    refactor(timeline): remove unused textColor and commented dot tint code

--- app/src/main/java/fr/ynryo/spotted/MarkerStopsDetailActivity.java
+++ app/src/main/java/fr/ynryo/spotted/MarkerStopsDetailActivity.java
@@ -450,7 +450,6 @@ public class MarkerStopsDetailActivity {
 
             // Tinte la barre avec la couleur du train
             int fillColor = Color.parseColor(vehicle.getFillColor() != null ? vehicle.getFillColor() : "#424242");
-            int textColor = Color.parseColor(vehicle.getTextColor() != null ? vehicle.getTextColor() : "#FFFFFF");
 
             View lineView = timelineView.findViewById(R.id.vLineBottom);
             if (lineView == null) lineView = timelineView.findViewById(R.id.vLineTop);
@@ -467,7 +466,6 @@ public class MarkerStopsDetailActivity {
 
             if (lineView != null)
                 ((GradientDrawable) lineView.getBackground().mutate()).setColor(fillColor);
-//                ((GradientDrawable) timelineView.findViewById(R.id.vStopDot).getBackground().mutate()).setColor(textColor);
         }
 
         private void bindPlatform(StopViewHolder vh, MarkerStop stop) {
